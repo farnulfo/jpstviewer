44e1f5950f8ab2cb824c7750ef830bc70868c1a5
Franck Arnulfo <franck.arnulfo@gmail.com>
2016-02-17 22:47:58 +0100

    Fix imports and default template comment

--- src/main/java/org/pipoware/jpstviewer/NDBItemBBTENTRY.java
+++ src/main/java/org/pipoware/jpstviewer/NDBItemBBTENTRY.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.pipoware.jpstviewer;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.pipoware.pst.exp.Block;
 import org.pipoware.pst.exp.PSTFile;
 import org.pipoware.pst.exp.pages.BBTENTRY;
 
